cf6efa15feb7f6cdf4a785da23e0dd3d89edb629
김선진 <tjswls8062@skuniv.ac.kr>
2023-07-19 12:40:22 +0900

    feat: 디자이너 회원가입 기능 추가

--- Backend/Chu/src/main/java/com/chu/designer/service/DesignerServiceImpl.java
+++ Backend/Chu/src/main/java/com/chu/designer/service/DesignerServiceImpl.java
@@ -1,11 +1,18 @@
 package com.chu.designer.service;
 
+import com.chu.customer.domain.CustomerDetailDto;
+import com.chu.customer.domain.FaceInfoDto;
 import com.chu.designer.domain.DesignerDetailDto;
 import com.chu.designer.domain.DesignerDto;
+import com.chu.global.domain.AlertDto;
+import com.chu.global.domain.BestDesignerDto;
 import com.chu.global.domain.SignInDto;
+import com.chu.worldcup.domain.WorldcupStatisticsInfoDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Service
@@ -54,6 +61,22 @@ public class DesignerServiceImpl implements DesignerService{
     @Override
     public DesignerDetailDto getDesignerDetail(String id) {
         DesignerDetailDto designerDetailDto = new DesignerDetailDto();
+
+        // 디자니어 정보
+        // 디자이너 테이블에서 정보 다 끌어오기
+
+        // 베스트 디자이너
+        // 디자이너 평점 limit 6
+        // 초기에 값이 없을 수도 있음 null 조심, 미리 dump 파일로 어느정도 데이터 삽입해놓기
+        ArrayList<BestDesignerDto> bestDesignerDtoList = new ArrayList<>();
+
+        // 월드컵 우승 데이터 사진들
+        ArrayList<WorldcupStatisticsInfoDto> worldcupStatisticsInfoDtoList = new ArrayList<>();
+
+        // 알림 데이터
+        // 디자이너 idx로 알림 접근
+        // 상담 IDX 토대로 APi 명세에 따른 로직 추가
+        ArrayList<AlertDto> alertDtoList = new ArrayList<>();
 
 
         return designerDetailDto;
