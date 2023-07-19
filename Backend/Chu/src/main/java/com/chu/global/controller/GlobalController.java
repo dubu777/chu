d1b54bc510facf8956d79141207b09719f2dd3e5
김선진 <tjswls8062@skuniv.ac.kr>
2023-07-19 09:32:40 +0900

    fix: 아이디, 이메일 중복 체크 독립 구조로 변경
    프론트에서 요청 두번 하기로 대화

--- Backend/Chu/src/main/java/com/chu/global/controller/GlobalController.java
+++ Backend/Chu/src/main/java/com/chu/global/controller/GlobalController.java
@@ -20,33 +20,5 @@ public class GlobalController {
     private final CustomerService customerService;
     private final DesignerService designerService;
 
-    // 로그인, 아이디 찾기, 비밀번호 찾기
-    // 회원, 디자이너 중복 체크 두번
-    @GetMapping("/sign-in")
-    public ResponseEntity<ResponseDto> signIn(SignInDto signInDto){
 
-        int isUser = 0;
-
-        // 디자이너 고객 구분할 flag 추가
-        // 고객일 경우
-        if(isUser == 1){
-            CustomerDetailDto customerDetailDto = customerService.getCustomerDetail(signInDto.getId());
-            ResponseDto responseDto = new ResponseDto(200, customerDetailDto);
-            return ResponseEntity.ok(responseDto);
-        }
-        // 일단 고객은 아닌 경우
-        else{
-            isUser = designerService.signIn(signInDto);
-            // 디자이너일 경우
-            if(isUser == 2){
-                DesignerDetailDto designerDetailDto = designerService.getDesignerDetail(signInDto.getId());
-                ResponseDto responseDto = new ResponseDto(200, designerDetailDto);
-                return ResponseEntity.ok(responseDto);
-            }
-        }
-        
-        // 로그인 실패
-        ResponseDto responseDto = new ResponseDto(200, null);
-        return ResponseEntity.ok(responseDto);
-    }
 }
