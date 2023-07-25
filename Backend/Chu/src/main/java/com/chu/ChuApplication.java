c7260c636fd83a13181bfbc433189fbd9df3d77b
김선진 <tjswls8062@skuniv.ac.kr>
2023-07-25 16:08:13 +0900

    Test: CI test2

--- Backend/Chu/src/main/java/com/chu/ChuApplication.java
+++ Backend/Chu/src/main/java/com/chu/ChuApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChuApplication {
 	public static void main(String[] args) {
-		// CI Test
+		// CI Test2
 		SpringApplication.run(ChuApplication.class, args);
 	}
 
