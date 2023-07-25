b8de240eee0d8f2c0df9268dba59b0968733e7e4
김선진 <tjswls8062@skuniv.ac.kr>
2023-07-25 21:06:49 +0900

    Test: correct branch push CI/CD test

--- Backend/Chu/src/main/java/com/chu/ChuApplication.java
+++ Backend/Chu/src/main/java/com/chu/ChuApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChuApplication {
 	public static void main(String[] args) {
-		// CI/CD Test2
+		// CI/CD Test2 correct branch push
 		SpringApplication.run(ChuApplication.class, args);
 	}
 
