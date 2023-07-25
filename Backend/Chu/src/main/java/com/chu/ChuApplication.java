24c509803d99c309101107952a6a8d89e59e5501
김선진 <tjswls8062@skuniv.ac.kr>
2023-07-25 21:52:22 +0900

    Test: wrong branch push test

--- Backend/Chu/src/main/java/com/chu/ChuApplication.java
+++ Backend/Chu/src/main/java/com/chu/ChuApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChuApplication {
 	public static void main(String[] args) {
-		// merge test2
+		// wrong branch test
 		SpringApplication.run(ChuApplication.class, args);
 	}
 
