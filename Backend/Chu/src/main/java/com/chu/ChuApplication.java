375937da68417957812c2f5d51e58ae98a52aa96
김선진 <tjswls8062@skuniv.ac.kr>
2023-07-25 15:42:42 +0900

    Test: 젠킨스 자동 배포 테스트

--- Backend/Chu/src/main/java/com/chu/ChuApplication.java
+++ Backend/Chu/src/main/java/com/chu/ChuApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChuApplication {
 	public static void main(String[] args) {
 		// merge test
+		// jenkinsTest
 		SpringApplication.run(ChuApplication.class, args);
 	}
 
