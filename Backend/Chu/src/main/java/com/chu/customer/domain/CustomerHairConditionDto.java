7452e15f16ac28f0ef48689180af2e181688d7d8
류민지 <i0692631@naver.com>
2023-07-28 22:42:03 +0900

    Feat: CustomerHairConditionDto 엔티티 작성

--- Backend/Chu/src/main/java/com/chu/customer/domain/CustomerHairConditionDto.java
+++ Backend/Chu/src/main/java/com/chu/customer/domain/CustomerHairConditionDto.java
@@ -1,5 +1,22 @@
 package com.chu.customer.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter @Setter
 public class CustomerHairConditionDto {
-    //Entity
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seq")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="seq")
+    private HairConditionDict hairConditionDict;
 }
