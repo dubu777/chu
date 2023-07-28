f37a95dd96f102ece4a0030aa3160a3a7fdd780f
류민지 <i0692631@naver.com>
2023-07-29 01:40:10 +0900

    Feat: Designer 엔티티 작성

--- Backend/Chu/src/main/java/com/chu/designer/domain/DesignerDto.java
+++ Backend/Chu/src/main/java/com/chu/designer/domain/DesignerDto.java
@@ -1,6 +1,50 @@
 package com.chu.designer.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
 public class DesignerDto {
 
-    // Entity
+    @Id @GeneratedValue
+    private int seq;
+
+    private String id;
+
+    private String name;
+
+    private String pwd;
+
+    private String email;
+
+    private char gender;
+
+    private String introduction;
+
+    private String certificationNum;
+
+    private String address;
+
+    private double latitude;
+
+    private double longitude;
+
+    private String salonName;
+
+    private String profileUploadImgName;
+
+    private String profileSavedImgName;
+
+    private float reviewScore;
+
+    private int cost;
+
+    private LocalDateTime createdDate;
+
 }
