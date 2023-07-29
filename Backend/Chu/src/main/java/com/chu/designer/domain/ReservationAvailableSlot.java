239e17cbb7be7bffeeae412c379e40fa5cc79ad9
류민지 <i0692631@naver.com>
2023-07-29 20:20:51 +0900

    Feat: 예약가능시간 엔티티 작성

--- Backend/Chu/src/main/java/com/chu/designer/domain/ReservationAvailableSlot.java
+++ Backend/Chu/src/main/java/com/chu/designer/domain/ReservationAvailableSlot.java
@@ -1,11 +1,12 @@
 package com.chu.designer.domain;
 
+import com.chu.consulting.domain.Consulting;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Getter @Setter
@@ -13,7 +14,23 @@ public class ReservationAvailableSlot {
 
     @Id
     @GeneratedValue
-    private Integer 
+    private Integer seq;
+
+    private String date;
+
+    private String time;
+
+    private Character state;
+
+    private LocalDateTime createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seq")
+    private Designer designer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seq")
+    private Consulting consulting;
 
 
 }
