2c96cfeed8ffbb118c2da295c8d143d841f7e3fb
류민지 <i0692631@naver.com>
2023-07-29 20:19:13 +0900

    Feat: DesignerPortfolio 엔티티 작성

---
+++ Backend/Chu/src/main/java/com/chu/designer/domain/DesignerPortfolio.java
@@ -0,0 +1,28 @@
+package com.chu.designer.domain;
+
+import com.chu.global.domain.ImagePath;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class DesignerPortfolio {
+
+    @Id @GeneratedValue
+    private Integer seq;
+
+    @Embedded
+    private ImagePath imagePath;
+
+    private Integer orders;
+
+    private LocalDateTime createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seq")
+    private Designer designer;
+
+}
