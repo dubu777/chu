b282cbd3d05aac0c66fb12bdd13db8de02a988c9
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-08-02 22:44:22 +0900

    Fix: 고객 마이페이지 CSS 수정

--- Frontend/src/components/CustomerComponent/ReserveList.jsx
+++ Frontend/src/components/CustomerComponent/ReserveList.jsx
@@ -24,7 +24,7 @@ const Hr = styled.div`
 const Wrap = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+
 `;
 const Wrapper = styled.div`
   display: flex;
@@ -34,7 +34,7 @@ const Wrapper = styled.div`
 `;
 const Box = styled.div`
   display: flex;
-  align-items: center;
+  
 `;
 
 const DetailBox = styled.div`
@@ -49,131 +49,44 @@ const ProfileBox = styled.div`
   justify-content: center;
   margin-left: 10px;
 `;
-// const InfoBox = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   margin-left: 10px;
-//   margin-top: -40px;
-// `;
-
 const Name = styled.p`
   font-size: 17px;
   font-weight: bold;
-  margin: 5px;
 `;
 
 const Icon = styled.img`
   width: 21px;
   margin-right: 3px;
-  margin-left: 5px;
 `;
 const StarBox = styled(motion.div)`
   width: 70px;
   height: 35px;
   display: flex;
-  /* justify-content: center; */
   align-items: center;
-  margin-top: 10px;
 `;
 const Time = styled.span`
   font-size: 16px;
   font-weight: bold;
   text-align: center;
-  margin-left: 10px;
 `;
 const Day = styled.span`
   font-size: 16px;
   font-weight: bold;
   text-align: center;
-  margin: 5px;
+
 `;
 const Text = styled.span`
   font-size: 16px;
   font-weight: bold;
   text-align: center;
-  margin-left: 5px;
 `;
 const CommentBox = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
-margin-top: 10px;
-margin-left: 20px;
+
 `;
 
-// const HashTag = styled.button`
-//   border: 0;
-//   border-radius: 0.3rem;
-//   background-color: #AF9571;
-//   color: #242321;
-//   height: 30px;
-//   margin-right: 10px;
-//   padding: 2px 15px;
-// `
-const CunsultBox = styled.div`
-  border: 0;
-  border-radius: 0.6rem;
-  background-color: #fff9e6;
-  width: 100%;
-  height: 450px;
-  padding-top: 70px;
-  padding-left: 30px;
-`;
-// const ResultBox = styled.div`
-//   border: 0;
-//   border-radius: 0.6rem;
-//   background-color: #4F472F;
-//   width: 70%;
-//   height: 280px;
-//   margin-left: 10px;
-//   margin-top: 10px;
-//   padding-top: 30px;
-//   padding-left: 30px;
-// `;
-
-const Letter = styled.p`
-  color : white;
-`;
-const ModalButton = styled.button`
-  /* 버튼 스타일링 */
-  background-color: #007bff;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
-`;
-
-const ModalWrapper = styled.div`
-  /* 모달 오버레이 스타일 */
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContent = styled.div`
-  /* 모달 스타일 */
-  background-color: white;
-  padding: 20px;
-  z-index: 9999;
-`;
-const boxVariants = {
-  nomal: {
-    scale: 1
-  },
-  hover: {
-    scale: 1.02,
-    transition: {
-      duration: 0.3,
-    },
-  }
-}
 const Overlay = styled(motion.div)`
   position: fixed;
   top: 0;
@@ -240,6 +153,10 @@ const ResultHr = styled.hr`
 const ReviewImg = styled.img`
   width: 23%;
 `;
+const RecentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 function ReserveList() {
   const {scrollY} = useScroll();
   const bigModalMatch = useMatch("customermypage/result/:designerSeq");
@@ -251,13 +168,7 @@ function ReserveList() {
   const onOverlayClick = () => {
     navigate('/customermypage');
   };
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  function openModal() {
-    setIsModalOpen(true);
-  };
-  function closeModal() {
-    setIsModalOpen(false);
-  };
+
   const data = {
     "name" : "소희",
     "consultingDate" : "07/11",
@@ -275,90 +186,106 @@ function ReserveList() {
         "img3.png"
     ]
   }
-  const pastConsuting = [
-    {
-        "consultingSeq" : 8,
-        "designerImg" : "img1.png",
-        "allReviewScore" : 4.7,
-        "name" : "지윤",
-        "consultingDate" : "05.21",
-        "consultingDateDay" : "금",
-        "consultingStartTime" : "18:30",
-        "consultingEndTime" : "17:00",
-        "myReviewScore" : 4.9,
-        "reviewContent" : "옛날에 남긴 나의 한줄평",
-    },];
 
-    const result = [{
-      "hairStyle" : [
-        {
-            "hairStyleSeq" : 1,
-            "hairStyleLabel" : "레이어드컷"
-        },
-        {
-            "hairStyleSeq" : 2,
-            "hairStyleLabel" : "히피펌"
-        },
-        {
-            "hairStyleSeq" : 3,
-            "hairStyleLabel" : "중단발"
-        }
+  const result = {
+    "customerSeq" : 1,
+    "name" : "김싸피",
+    "id" : "ssafy",
+    "email" : "ssafy@gmail.com",
+    "img" : "img1.png",
+    "hairCondition" : [
+        "얇은 모발",
+        "굵은 모발"
     ],
-    "imgs" : [
+    "face" : "계란형",
+    "futureConsulting" : [
         {
-            "imgSeq" : 1,
-            "img" : "img1.png"
+            "consultingSeq" : 10,
+            "designerImg" : "img1.png",
+            "reviewScore" : 4.7,
+            "name" : "지윤",
+            "consultingDate" : "08.21",
+            "consultingDateDay" : "금",
+            "consultingStartTime" : "18:30",
+            "url" : " ",
         },
         {
-            "imgSeq" : 2,
-            "img" : "img2.png"
+            "consultingSeq" : 11,
+            "designerImg" : "img2.png",
+            "reviewScore" : 4.8,
+            "name" : "민지",
+            "consultingDate" : "08.22",
+            "consultingDateDay" : "금",
+            "consultingStartTime" : "18:30",
+            "url" : " ",
+        }
+    
+    ],
+    "pastConsulting" : [
+        {
+            "consultingSeq" : 8,
+            "designerImg" : "icon/designerimg.png",
+            "allReviewScore" : 4.7,
+            "name" : "지윤",
+            "consultingDate" : "05.21",
+            "consultingDateDay" : "금",
+            "consultingStartTime" : "18:30",
+            "consultingEndTime" : "17:00",
+            "myReviewScore" : 4.9,
+            "reviewContent" : "최고에요!",
         },
         {
-            "imgSeq" : 3,
-            "img" : "img3.png"
+            "consultingSeq" : 9,
+            "designerImg" : "icon/designerimg.png",
+            "allReviewScore" : 4.7,
+            "name" : "민지",
+            "consultingDate" : "06.21",
+            "consultingDateDay" : "금",
+            "consultingStartTime" : "18:30",
+            "consultingEndTime" : "17:00",
+            "myReviewScore" : 4.9,
+            "reviewContent" : "추천합니다!",
         },
-    ]
-}]
+      ]
+    }
 
   return (
     <Container>
       <Hr/>
-      <Wrap>
-      <Wrapper>
-        <Box>
-          <ProfileBox>
-          <DesignerImg src="./icon/designerimg.png"/>
-            <StarBox>
-              <Icon src="./icon/star.png"/>
-              <Text>4.8</Text>
-            </StarBox> 
-          </ProfileBox>
-        </Box>
-        <InfoBox>
-          <Name>재현 디자이너</Name>
+      {result.pastConsulting.map((data) => (
+        <Wrap>
+          <Wrapper>
+            <Box>
+              <ProfileBox>
+                <DesignerImg src={data.designerImg}/>
+                <StarBox>
+                  <Icon src="./icon/star.png"/>
+                  <Text>4.8</Text>
+                </StarBox> 
+              </ProfileBox>
+              <InfoBox>
+                <Name>{data.name}</Name>
+                <Box>
+                  <StarBox>
+                    <Icon src="./icon/star.png"/>
+                    <Text>{data.myReviewScore}</Text>
+                  </StarBox>
+                  <CommentBox>
+                    <Text>{data.reviewContent}</Text>
+                  </CommentBox>
+                </Box>
+                <DetailBox >
+                  <Text onClick={() =>onBoxClicked(data.designerSeq)}>상담 결과 보기</Text>
+                </DetailBox>
+              </InfoBox>
+            </Box>
+          </Wrapper>
           <Box>
-            <Day>{pastConsuting[0].consultingDate} ({pastConsuting[0].consultingDateDay})</Day>
-            <Time>{pastConsuting[0].consultingStartTime} ~ {pastConsuting[0].consultingEndTime}</Time>
-            {/* <Day>7/14(금)</Day>
-            {personA && <Time>{personA.consultingStartTime} ~ {getPlus30(personA.consultingStartTime)} </Time>} */}
+            <Day>{data.consultingDate}({data.consultingDateDay})</Day>
+            <Time>{data.consultingStartTime} ~ {data.consultingEndTime}</Time>
           </Box>
-
-          <Box>
-            <StarBox>
-              <Icon src="./icon/star.png"/>
-              <Text>4.8</Text>
-            </StarBox>
-            <CommentBox>
-              {/* <Icon src="./icon/money.png"/> */}
-              <Text>{pastConsuting[0].reviewContent}</Text>
-            </CommentBox>
-          </Box>
-        </InfoBox>
-      </Wrapper>
-        <DetailBox >
-          <Text onClick={() =>onBoxClicked(data.designerSeq)}>상담 결과 보기</Text>
-        </DetailBox>
-      </Wrap>
+        </Wrap>
+      ))}
       <AnimatePresence>
         { bigModalMatch ? (
           <>
