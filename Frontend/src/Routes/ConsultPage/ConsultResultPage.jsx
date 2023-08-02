dbc2f371cd56487d0c38eb6c9ab27ba401570134
wldbs8241 <wldbs8241@naver.com>
2023-08-02 13:33:35 +0900

    Feat: 디자이너 상담 결과 작성 페이지 생성

---
+++ Frontend/src/Routes/ConsultPage/ConsultResultPage.jsx
@@ -0,0 +1,30 @@
+// 상담 종료 후 디자이너가 상담 결과 입력하는 페이지
+// 입력할 때 selectedHairStyle에 커트랑 펌 종류 범위
+
+import React from "react";
+import { styled } from "styled-components";
+import { useState } from "react";
+
+const Container = styled.div`
+
+`;
+
+const Wrapper = styled.div `
+  
+`;
+const Box = styled.div`
+
+`;
+
+function ConsultResultPage(){
+  return(
+    <Container>
+      <Wrapper>
+        <Box></Box>
+      </Wrapper>
+    </Container>
+
+  );
+}
+
+export default ConsultResultPage;
