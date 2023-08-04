d4b783f9515965558f37330ca786086cee8e5bbb
wldbs8241 <wldbs8241@naver.com>
2023-08-04 15:58:39 +0900

    Test: 통신 코드 수정

--- Frontend/src/apis/customer.js
+++ Frontend/src/apis/customer.js
@@ -5,13 +5,30 @@ const BASE_URL = 'https://i9b111.q.ssafy.io:9090/api';
 
 
 // 이미지 첨부 api
-export const attachImage = async(seq, formData)=> {
+// export const attachImage = async(seq, formData)=> {
+//     try {
+//         const response = await axios.patch(`${BASE_URL}/customer/detail/img/${seq}`, {
+//            "img" : formData,
+//            headers: {
+//             'Content-Type': 'multipart/form-data'
+//         }
+//         })
+//         return response.data.result;
+//     } catch(error) {
+//         throw new Error('이미지 보내기 실패')
+//     }
+// };
+
+
+export const attachImage = async (seq, formData) => {
     try {
-        const response = await axios.patch(`${BASE_URL}/customer/detail/img/${seq}`, {
-           "img" : formData
-        })
+        const response = await axios.patch(`${BASE_URL}/customer/detail/img/${seq}`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
         return response.data.result;
-    } catch(error) {
-        throw new Error('이미지 보내기 실패')
+    } catch (error) {
+        throw new Error('이미지 보내기 실패');
     }
 };
