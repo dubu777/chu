237cba589deb951f21023ef3c92592f9fb7f0426
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-08-07 02:35:21 +0900

    Test: 이미지 저장 테스트

--- Backend/Chu/src/main/java/com/chu/customer/service/CustomerDetailServiceImpl.java
+++ Backend/Chu/src/main/java/com/chu/customer/service/CustomerDetailServiceImpl.java
@@ -27,12 +27,19 @@ public class CustomerDetailServiceImpl implements CustomerDetailService {
 
     @Override
     public String getSavedImgFilePath(Integer customerSeq, MultipartFile file) throws IOException {
+
+        // 우분투 서버 저장 경로
         String uploadDir = "/chu/upload/images/customer/";
+        // 원본 파일명
         String originalFilename = file.getOriginalFilename();
+        // 저장될 고유 파일명
         String fileName = customerSeq + "_" + originalFilename;
 
+        // 저장될 경로로 디렉토리 객체 생성
         File directory = new File(uploadDir);
+        // 저장될 경로 + 파일명
         String filePath = uploadDir + fileName;
+        // 저장될 경로로 파일 객체 생성
         File destFile = new File(filePath);
         System.out.println(filePath);
 
@@ -46,8 +53,10 @@ public class CustomerDetailServiceImpl implements CustomerDetailService {
         }
 
         try {
+            // 파일 지정해놓은 경로에 저장
             file.transferTo(destFile);
             log.info("서비스 >>> 파일 저장 성공! filePath : " + filePath);
+            // 저장된 파일 경로 리턴 -> 여기서 db에 저장해야되는 게 정확히 뭐와 뭔지 알아야할듯.
             return filePath;
         } catch (IOException e) {
             log.error("파일 저장 실패:", e);
@@ -59,13 +68,12 @@ public class CustomerDetailServiceImpl implements CustomerDetailService {
     @Transactional
     public Boolean patchImage(Integer customerSeq, String fileName) {
 
+        // 현재 고객 엔티티 호출. 이거 이렇게 하면 고객 나오나? -> 찍어보기 암튼 저장되는듯
         Customer customer = customerDetailRepository.getById(customerSeq);
-
-
 
         // 저장
         customer.getImagePath().setUploadImgName(fileName);
-        customer.getImagePath().setSavedImgName(fileName);
+        //customer.getImagePath().setSavedImgName(fileName);  // 원본파일명 저장은 안될듯.
 
         return true;
     }
