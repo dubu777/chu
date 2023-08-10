9aa7a8c1a3271305dbad1973481ac54a99c06add
wldbs8241 <wldbs8241@naver.com>
2023-08-11 01:03:01 +0900

    Fix: 마이페이지 프로필 사진 렌더링 오류 확인

--- Frontend/src/recoil/reservation.js
+++ Frontend/src/recoil/reservation.js
@@ -1,1 +1,19 @@
+import { atom, useRecoilState } from 'recoil';
 
+// 예약 정보를 각각 Atom에 정의(예약일, 시간, 메모)
+export const reserveInfo = atom({
+    key: 'customerSeq',
+    default: {
+      customerSeq: null,
+      designerSeq: null,
+      date: null,
+      time: null,
+      consultingMemo: '',
+      portfolios: []
+    },
+  });
+
+const consultImg = atom({
+  key: 'selectedFile',
+  default: null,
+});
