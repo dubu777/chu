6c496b8980079db5c2609eb08fd78f62808f0741
hajin618 <1998rlagkwls@gmail.com>
2023-08-10 15:55:07 +0900

    Feat: 상담 참여하기 리턴값 수정

--- Backend/Chu/src/main/java/com/chu/consulting/repository/ConsultingTargetInfoRepository.java
+++ Backend/Chu/src/main/java/com/chu/consulting/repository/ConsultingTargetInfoRepository.java
@@ -3,5 +3,9 @@ package com.chu.consulting.repository;
 import com.chu.consulting.domain.ConsultingTargetInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConsultingTargetInfoRepository extends JpaRepository<ConsultingTargetInfo, Integer> {
+
+    List<ConsultingTargetInfo> findAllByConsultingSeq(int consultingSeq);
 }
