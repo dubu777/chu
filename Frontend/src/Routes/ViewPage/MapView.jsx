58909682e42ccf1d0abf65dafeed1244e50aa1c7
hajin618 <1998rlagkwls@gmail.com>
2023-08-11 22:33:36 +0900

    Feat: 카카오맵 생성

--- Frontend/src/Routes/ViewPage/MapView.jsx
+++ Frontend/src/Routes/ViewPage/MapView.jsx
@@ -1,20 +1,38 @@
 // 여기는 주변 디자이너 찾기 지도 페이지
 
-import { styled } from "styled-components";
+/*global kakao*/
+// import { styled } from "styled-components";
 import { useState, useEffect, useRef } from "react";
 
-const Container = styled.div`
+const { kakao } = window;
+// const Container = styled.div`
     
-`;
+// `;
 
 function MapView () {
+
+    useEffect(() => {
+        // console.log("mapview");
+        const container = document.getElementById('map'); // 지도 담을 영역의 dom 레퍼런스
+        const options = {
+            center : new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+            level: 3
+        };
+        const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+
+    }, [])
+
+
     return(
-        <Container>
+        <div>
             <p>여기는 주변 디자이너 찾기 지도 페이지</p>
-
-
-
-        </Container>
+            <div id="map" style={{
+                width: "800px",
+                height: "600px"
+            }}>
+            </div>
+        </div>
+        
     );
 };
 
